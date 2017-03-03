94ed9cdf75645d259924f522288246d79ed5f306
Jakob Makovac <jakob.makovac@gmail.com>
2017-03-03 15:47:44 +0100

    Added method for palindrome checking to EP4

--- src/EP4.java
+++ src/EP4.java
@@ -3,6 +3,20 @@
  */
 public class EP4 {
     public static void main(String[] args) {
-        
+        isPalindrome(303031);
+    }
+
+    private static boolean isPalindrome (int x){
+        String numString = String.valueOf(x);
+        int numLength = numString.length();
+
+        for (int i = 0; i < numLength; i++){
+            if(numString.charAt(i) != numString.charAt(numLength-i-1)){
+                System.out.println("Number is not a palindrome.");
+                return false;
+            }
+        }
+        System.out.println("Number is a palindrome.");
+        return true;
     }
 }
